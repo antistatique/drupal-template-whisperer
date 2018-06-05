9ad09424593091109abb085e55450b36bf86e406
Kevin Wenger <wenger.kev@gmail.com>
2018-06-05 15:47:10 +0200

    improve UI Page testes coverage

--- tests/src/Functional/UiPageTest.php
+++ tests/src/Functional/UiPageTest.php
@@ -25,12 +25,41 @@ class UiPageTest extends TemplateWhispererTestBase {
   protected $profile = 'minimal';
 
   /**
+   * The Entity Type Manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
    * {@inheritdoc}
    */
   protected function setUp() {
     parent::setUp();
 
-    // Create a user for tests.
+    /** @var \Drupal\Core\Entity\EntityTypeManager $entityTypeManager */
+    $this->entityTypeManager = $this->container->get('entity_type.manager');
+
+    // Create an page content type.
+    $this->drupalCreateContentType(['type' => 'page', 'name' => 'Basic Page']);
+
+    // Set up our Template Whisperer field on the Basic Page content type.
+    $this->entityTypeManager
+      ->getStorage('field_storage_config')
+      ->create([
+        'field_name'  => 'field_template_whisperer',
+        'entity_type' => 'node',
+        'type'        => 'template_whisperer',
+      ])->save();
+    $this->entityTypeManager
+      ->getStorage('field_config')
+      ->create([
+        'entity_type' => 'node',
+        'field_name'  => 'field_template_whisperer',
+        'bundle'      => 'page',
+      ])->save();
+
+    // Create a user for tests & logged in.
     $account = $this->drupalCreateUser(['administer template whisperer suggestion entities']);
     $this->drupalLogin($account);
   }
@@ -100,7 +129,7 @@ class UiPageTest extends TemplateWhispererTestBase {
    * Tests that the usage page works.
    */
   public function testUsagePage() {
-    $suggestion = $this->container->get('entity_type.manager')->getStorage('template_whisperer_suggestion')
+    $suggestion = $this->entityTypeManager->getStorage('template_whisperer_suggestion')
       ->create([
         'id'         => 'test',
         'name'       => 'Test Template Whisperer',
@@ -109,13 +138,10 @@ class UiPageTest extends TemplateWhispererTestBase {
     $suggestion->save();
 
     // Create a Basic Page content type that we will use for testing.
-    $this->drupalCreateContentType(['type' => 'page', 'name' => 'Basic Page']);
-
-    $page = $this->container->get('entity_type.manager')->getStorage('node')
-      ->create([
-        'type'  => 'page',
-        'title' => 'Basic Page',
-      ]);
+    $page = $this->entityTypeManager->getStorage('node')->create([
+      'type'  => 'page',
+      'title' => 'Basic Page',
+    ]);
     $page->save();
 
     $this->drupalGet('admin/structure/template-whisperer/test/usage');
@@ -124,15 +150,57 @@ class UiPageTest extends TemplateWhispererTestBase {
     // Test that there is an empty listing.
     $this->assertSession()->pageTextContains('This suggestion has not been currently used.');
 
-    $suggestion_usage = $this->container->get('template_whisperer.suggestion.usage');
-    $suggestion_usage->add($suggestion, 'template_whisperer', 'node', $page->id());
+    // Create a page & attache a suggestion again.
+    $page = $this->entityTypeManager->getStorage('node')->create([
+      'type'  => 'page',
+      'title' => 'Basic Page N°2',
+      'field_template_whisperer' => 'test',
+    ]);
+    $page->save();
+
+    // Asserts that attaching a suggestion to a Template whisperer field int0
+    // an entity (here a basic page), it will increment the usage count.
     $this->drupalGet('admin/structure/template-whisperer/test/usage');
     $this->assertSession()->statusCodeEquals(200);
-
-    $this->assertSession()->elementContains('css', 'table', 'Basic Page');
+    $this->assertSession()->elementContains('css', 'table', 'Basic Page N°2');
     $this->assertSession()->elementContains('css', 'table', 'node');
     $this->assertSession()->elementContains('css', 'table', 'template_whisperer');
     $this->assertSession()->elementContains('css', 'table', '1');
+  }
+
+  /**
+   * Tests that deleting when already used show a warning message to the user.
+   */
+  public function testDeleteWhenUse() {
+    // Create a default Template Whisperer.
+    // Set up our entity_type and user type for the field.
+    $this->entityTypeManager->getStorage('template_whisperer_suggestion')->create([
+      'id'         => 'test',
+      'name'       => 'Test Template Whisperer',
+      'suggestion' => 'test',
+    ])->save();
+
+    // Create a default page & attache a suggestion.
+    $node = $this->entityTypeManager->getStorage('node')->create([
+      'type'  => 'page',
+      'title' => 'Basic Page',
+      'field_template_whisperer' => 'test',
+    ]);
+    $node->save();
+
+    $this->drupalGet('admin/structure/template-whisperer/test/delete');
+    $this->assertSession()->pageTextContains('The suggestion "Test Template Whisperer" is used in 1 place. Any usage of this suggestion will be lost. This action cannot be undone.');
+
+    // Create a second page & attache a suggestion again.
+    $node = $this->entityTypeManager->getStorage('node')->create([
+      'type'  => 'page',
+      'title' => 'Basic Page N°2',
+      'field_template_whisperer' => 'test',
+    ]);
+    $node->save();
+
+    $this->drupalGet('admin/structure/template-whisperer/test/delete');
+    $this->assertSession()->pageTextContains('The suggestion "Test Template Whisperer" is used in 2 places. Any usage of this suggestion will be lost. This action cannot be undone.');
   }
 
 }
